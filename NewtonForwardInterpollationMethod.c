79fd2306875f601c65a55780da60e24faba3681b
PRITAM KUMAR MANI <pritammanijoy2001@gmail.com>
2022-09-19 23:32:08 +0530

    #refresh

--- NewtonForwardInterpollationMethod.c
+++ NewtonForwardInterpollationMethod.c
@@ -1,53 +1,40 @@
-//Newton Forward Interploation Method
 #include<stdio.h>
-#include<math.h>
-
-int main()
-{
-    float x,u1,u,y;
-    int i,j,n,fact;
-    printf("Enter no. of terms\n");
+int main(){
+    int i,j,n;
+    float x[25],y[25][25];
+    printf("Enter the no. of Interpolating Point :");
     scanf("%d",&n);
-    float a[n][n+1];
-    printf("Enter Values of X\n");
-    for(i=0;i<n;i++)
-          scanf("%f",&a[i][0]);
-    printf("Enter Values of Y\n");
-    for(i=0;i<n;i++)
-          scanf("%f",&a[i][1]); 
-    printf("Enter value of x for which you want y\n");
-    scanf("%f",&x);
-
-    //Find the Difference Table    
-    for(j=2;j<n+1;j++)
+    for ( i = 0; i < n; i++)
     {
-        for(i=0;i<n-j+1;i++)
-            a[i][j] = a[i+1][j-1]-a[i][j-1];
+        printf("Enter the  Value x[%d]:",i);
+        scanf("%f",&x[i]);
     }
-
-    //Print the Difference Table
-    printf("The Difference Table is as follows:\n");
-    for(i=0;i<n;i++)
+    for ( i = 0; j < n; j++)
     {
-     for(j=0;j<=n-i;j++)
-        printf("%f ",a[i][j]);
-     printf("\n");
+      printf("Enter Corresponding value of y[0][%d]:",j);
+      scanf("%f",&y[0][j]);    
     }
+    for ( i = 1; i < n; i++)
+    {
+        for ( j = 0; j < n-i; j++)
+        {
+            y[i][j]=y[i-1][j+1]-y[i-1][j];
+        }
+        
+    }
+    printf("Forward Differences are :\n");
+    for ( i = 0; i < n; i++)
+    {
+        printf("%f\t",x[i]);
+        {
+            for ( j = 0; j < n-i; j++)
+            {
+                printf("%f\t",y[j][i]);
+            }
+            
+        }
+        printf("\n");
+    }
+    return 0;
     
-    //find u
-    u= (x - a[0][0])/(a[1][0]-a[0][0]);
-    //Initial y, u1, k
-    y=a[0][1]; 
-    u1=u;
-    fact=1;
-    for(i=2;i<=n;i++)
-    {
-        y=y+(u1*a[0][i])/fact;
-        //Update k, u1
-        fact=fact*i; 
-        u1=u1*(u-(i-1));
-    }
-    printf("\n\nValue at X=%g is = %f", x,y);
-
-
 }
